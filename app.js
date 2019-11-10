20e5857b74f0c519442a6a41cec36d17d8e067e4
Ubuntu <ubuntu@ip-172-31-85-125.ec2.internal>
2019-11-10 16:20:12 +0000

    Minor changes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 window.onload= function() {
     var button  = document.getElementById("btn");
+    var search = document.getElementById("srchbar").value;
     //const fetch = require('node-fetch');
     
     button.addEventListener("click", function(){
         fetch('superheroes.php')
         .then(response => response.text())
         .then (data => {
-          alert(data);  
+          //alert(data);  
+         data.forEach(i =>{
+             
+         })
+          
         })
         
         
